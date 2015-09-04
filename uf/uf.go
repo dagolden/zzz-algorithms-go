7195f0e774a79319d5ae11a598e1627abfb705cf
David Golden <dagolden@cpan.org>
2015-09-03 20:22:21 -0400

    QuickFind implementation complete

--- uf/uf.go
+++ uf/uf.go
@@ -28,10 +28,51 @@ func NewQuickFind(n int) *QuickFind {
 	if n < 1 {
 		return nil
 	}
-	return &QuickFind{make([]int, n), 0}
+	nl := make([]int, n)
+	for i, _ := range nl {
+		nl[i] = i
+	}
+	return &QuickFind{nl, n}
 }
 
 // Size returns the number of nodes
-func (u QuickFind) Size() int {
+func (u *QuickFind) Size() int {
 	return len(u.nodes)
+}
+
+// Count returns the number of components
+func (u *QuickFind) Count() int {
+	return u.count
+}
+
+// Find returns component for node n
+func (u *QuickFind) Find(n int) int {
+	return u.nodes[n]
+}
+
+// Connected returns true if p and q are connected and false otherwise
+func (u *QuickFind) Connected(q, p int) bool {
+	return u.Find(p) == u.Find(q)
+}
+
+// Union joins p and q into a single component
+func (u *QuickFind) Union(p, q int) {
+	pID, qID := u.Find(p), u.Find(q)
+
+	// already connected
+	if pID == qID {
+		return
+	}
+
+	// change pID components to qID
+	for i, v := range u.nodes {
+		if v == pID {
+			u.nodes[i] = qID
+		}
+	}
+
+	// one less component now
+	u.count--
+
+	return
 }
--- uf/uf_test.go
+++ uf/uf_test.go
@@ -11,17 +11,40 @@ import (
 	"testing"
 )
 
-func TestConstruction(t *testing.T) {
-	// test that constructed size is correct
-	qf := uf.NewQuickFind(10)
-	if qf.Size() != 10 {
-		t.Error("QuickFind nodes had incorrect size ", qf.Size())
-	}
+func TestQuickFind(t *testing.T) {
 
-	// test that arguments must be positive integers
+	// test that constructor arguments must be positive integers
 	for i := 0; i > -2; i-- {
 		if uf.NewQuickFind(i) != nil {
 			t.Errorf("NewQuickFind(%d) was non-nil", i)
 		}
+	}
+
+	// test that constructed size is correct
+	qf := uf.NewQuickFind(10)
+	if qf.Size() != 10 {
+		t.Error("QuickFind had incorrect size ", qf.Size())
+	}
+	if qf.Count() != 10 {
+		t.Errorf("QuickFind had incorrect count. Got %d, expected %d", qf.Count(), 10)
+	}
+
+	// test find returns identity before union
+	for i := 0; i < 10; i++ {
+		if qf.Find(i) != i {
+			t.Errorf("node %d not linked to self", i)
+		}
+	}
+
+	// test union
+	if qf.Count() != 10 {
+		t.Errorf("pre-union component count: want %d, got %d", 10, qf.Count())
+	}
+	qf.Union(0, 1)
+	if !qf.Connected(0, 1) {
+		t.Errorf("nodes 0 and 1 not connected")
+	}
+	if qf.Count() != 9 {
+		t.Errorf("post-union component count: want %d, got %d", 9, qf.Count())
 	}
 }
